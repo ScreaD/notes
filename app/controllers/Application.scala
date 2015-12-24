b22bb947accdbc737542c0eb11545ae224a3c807
scread <pavlosevidov@gmail.com>
2015-12-24 23:14:23 +0200

    Resolved problem with multiple html.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,10 +4,9 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  val message = "Fuck this SITE"
 
   def index = Action {
-    Ok(views.html.index(message))
+    Ok(views.html.index())
   }
 
 }
